7fb424991c4836ebd99b6a315a098ae054ec2577
Abdullahi Odesanmi <abdulodesanmi@gmail.com>
2022-12-21 00:18:16 +0100

    (refactor): component dynamic imports

--- src/router/index.js
+++ src/router/index.js
@@ -1,55 +1,54 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
-import Search from '../views/Search.vue'
-import Library from '../views/Library.vue'
-import NowPlaying from '../components/NowPlaying.vue'
-import HandleCallback from '../views/HandleCallback.vue'
-// import Album from '../views/Album.vue'
+import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/search',
-    name: 'Search',
-    component: Search
-  },
-  {
-    path: '/Library',
-    name: 'Library',
-    component: Library
-  },
-  {
-    path: '/playing',
-    name: 'Playing',
-    component: NowPlaying
-  },
-  {
-    path: '/callback',
-    name: 'Callback',
-    component: () => import('../views/HandleCallback.vue')
-  },
-  // {
-  //   path: '/album',
-  //   name: 'Album',
-  //   component: Album
-  // }
-  // {
-  //   path: '/about',
-  //   name: 'About',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  // }
-]
+	{
+		path: "/",
+		name: "Welcome",
+		component: () => import("../views/Welcome.vue"),
+	},
+	{
+		path: "/home",
+		name: "Home",
+		component: () => import("../views/Home.vue"),
+	},
+	{
+		path: "/search",
+		name: "Search",
+		component: () => import("../views/Search.vue"),
+	},
+	{
+		path: "/Library",
+		name: "Library",
+		component: () => import("../views/Library.vue"),
+	},
+	{
+		path: "/playing",
+		name: "Playing",
+		component: () => import("../components/NowPlaying.vue"),
+	},
+	{
+		path: "/callback",
+		name: "Callback",
+		component: () => import("../views/HandleCallback.vue"),
+	},
+	// {
+	//   path: '/album',
+	//   name: 'Album',
+	//   component: Album
+	// }
+	// {
+	//   path: '/about',
+	//   name: 'About',
+	//   // route level code-splitting
+	//   // this generates a separate chunk (about.[hash].js) for this route
+	//   // which is lazy-loaded when the route is visited.
+	//   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+	// }
+];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes
-})
+	history: createWebHistory(import.meta.env.BASE_URL),
+	routes,
+});
 
-export default router
+export default router;
