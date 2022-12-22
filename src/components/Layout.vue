0d3cca2a1139ef82bb99a234049b903286336419
Abdullahi Odesanmi <abdulodesanmi@gmail.com>
2022-12-22 09:24:45 +0100

    (refactor): setup layout and protected route

---
+++ src/components/Layout.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { defineAsyncComponent, onBeforeMount } from 'vue';
+import { useStore } from 'vuex';
+
+const store = useStore();
+
+const SideBar = defineAsyncComponent(() =>
+    import('./Sidebar.vue')
+);
+
+const Header = defineAsyncComponent(() =>
+    import('./Header.vue')
+);
+
+onBeforeMount(async () => {
+    await store.dispatch('getUserInfo');
+  });
+</script>
+
+<template>
+    <div>
+        <SideBar />
+        <Header />
+        <slot />
+    </div>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,49 +1,51 @@
 import { createRouter, createWebHistory } from "vue-router";
 
+const checkAccess = (to, from, next) => {
+	if (localStorage.getItem("rscAccessToken")) {
+		next();
+	} else {
+		next("/welcome");
+	}
+}
+
 const routes = [
 	{
 		path: "/",
+		component: () => import("../components/Layout.vue"),
+		beforeEnter: checkAccess,
+		children: [
+			{
+				path: "/",
+				name: "Home",
+				component: () => import("../views/Home.vue"),
+			},
+			{
+				path: "/search",
+				name: "Search",
+				component: () => import("../views/Search.vue"),
+			},
+			{
+				path: "/library",
+				name: "Library",
+				component: () => import("../views/Library.vue"),
+			},
+			{
+				path: "/playing",
+				name: "Playing",
+				component: () => import("../components/NowPlaying.vue"),
+			},
+		],
+	},
+	{
+		path: "/welcome",
 		name: "Welcome",
 		component: () => import("../views/Welcome.vue"),
-	},
-	{
-		path: "/home",
-		name: "Home",
-		component: () => import("../views/Home.vue"),
-	},
-	{
-		path: "/search",
-		name: "Search",
-		component: () => import("../views/Search.vue"),
-	},
-	{
-		path: "/library",
-		name: "Library",
-		component: () => import("../views/Library.vue"),
-	},
-	{
-		path: "/playing",
-		name: "Playing",
-		component: () => import("../components/NowPlaying.vue"),
 	},
 	{
 		path: "/callback",
 		name: "Callback",
 		component: () => import("../views/HandleCallback.vue"),
 	},
-	// {
-	//   path: '/album',
-	//   name: 'Album',
-	//   component: Album
-	// }
-	// {
-	//   path: '/about',
-	//   name: 'About',
-	//   // route level code-splitting
-	//   // this generates a separate chunk (about.[hash].js) for this route
-	//   // which is lazy-loaded when the route is visited.
-	//   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-	// }
 ];
 
 const router = createRouter({
