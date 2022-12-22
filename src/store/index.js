8a60a4c4cb060d22df45dc4cb5ee728e1d26836f
Abdullahi Odesanmi <abdulodesanmi@gmail.com>
2022-12-22 12:01:31 +0100

    (chore): fetch top albums

--- src/store/index.js
+++ src/store/index.js
@@ -1,22 +1,34 @@
-import { createStore, Store } from 'vuex'
-import { ref, inject } from 'vue'
+import { createStore, Store } from "vuex";
+import { ref, inject } from "vue";
 
 export default createStore({
   state: {
-    userInfo: ref({})
+    userInfo: ref({}),
+    topPlayed: ref([]),
   },
+
   mutations: {
     SET_USER(state, payload) {
       state.userInfo = payload;
-    }
+    },
+    SET_TOP_PLAYED(state, payload) {
+      state.topPlayed = payload;
+    },
   },
+
   actions: {
     async getUserInfo({ commit }) {
       const $axios = inject("useAxios");
       const { data } = await $axios.get("/me");
       commit("SET_USER", data);
     },
+
+    async getTopPlayed({ commit }) {
+      const $axios = inject("useAxios");
+      const { data: response } = await $axios.get("/me/top/tracks");
+      const topPlayed = response.items.slice(-6);
+      commit("SET_TOP_PLAYED", topPlayed);
+    },
   },
-  modules: {
-  }
-})
+  modules: {},
+});
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,16 @@
 <script setup>
+import { onBeforeMount, computed } from "vue";
+import { useStore } from "vuex";
 
+const store = useStore();
+
+const topAlbums = computed(() => {
+    return store.state.topPlayed;
+});
+
+onBeforeMount(async () => {
+    await store.dispatch("getTopPlayed");
+});
 </script>
 
-<template>
-  
-</template>
+<template></template>
