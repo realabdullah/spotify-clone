b11a12124ccc0dfc317275a8d66c3057597a432a
Abdullahi Odesanmi <abdulodesanmi@gmail.com>
2022-12-22 09:23:02 +0100

    (chore): clean entry SFC

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,6 @@
 <script setup>
-import { defineAsyncComponent } from 'vue';
-
-const SideBar = defineAsyncComponent(() =>
-  import('./components/Sidebar.vue')
-);
-
-const Header = defineAsyncComponent(() =>
-  import('./components/Header.vue')
-);
 </script>
 
 <template>
-  <SideBar />
-  <Header />
-  <router-view />
+		<router-view />
 </template>
