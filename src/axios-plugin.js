2c917e229e25bd4fe118e5e2828f2b7a56f7a5da
Abdullahi Odesanmi <abdulodesanmi@gmail.com>
2022-12-22 07:48:14 +0100

    (feat): setup interceptor

--- src/axios-plugin.js
+++ src/axios-plugin.js
@@ -1,9 +1,12 @@
 import axios from "axios";
+import { useRequestToken } from "./composables/requestToken.js";
+
+const { requestToken } = useRequestToken();
 
 export default {
 	install(app) {
-        const rscAccessToken = localStorage.getItem('rscAccessToken');
-		var useAxios = axios.create({
+		const rscAccessToken = localStorage.getItem("rscAccessToken");
+		const useAxios = axios.create({
 			baseURL: import.meta.env.VITE_API_URL,
 			headers: {
 				"Content-Type": "application/json",
@@ -11,6 +14,21 @@ export default {
 			},
 		});
 
-        app.provide("useAxios", useAxios);
+		useAxios.interceptors.response.use(
+			(response) => {
+				return response;
+			}, 
+			async (error) => {
+				if (error.response.status === 401) {
+					await requestToken();
+					const config = error.config;
+					config.headers["Authorization"] = `Bearer ${localStorage.getItem("rscAccessToken")}`;
+					return useAxios(config);
+				}
+				return Promise.reject(error);
+			}
+		);
+
+		app.provide("useAxios", useAxios);
 	},
 };
