3ef89a86097bf1aecd5418ed041b43fc4decc71a
Abdullahi Odesanmi <abdulodesanmi@gmail.com>
2022-12-22 12:00:26 +0100

    (fix): expand scopes

--- src/composables/login.js
+++ src/composables/login.js
@@ -2,7 +2,14 @@ export function useLogin() {
 	const login = () => {
 		const CLIENT_ID = import.meta.env.VITE_CLIENT_ID;
 		const REDIRECT_URI = import.meta.env.VITE_REDIRECT_URI;
-		const scopes = ["user-read-private", "user-read-email"];
+		const scopes = [
+			"user-read-private",
+			"user-read-email",
+			"user-top-read",
+			"user-read-recently-played",
+			"user-library-read",
+			"playlist-read-private",
+		];
 		const authorizeUrl = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=${encodeURIComponent(
 			REDIRECT_URI
 		)}&scope=${encodeURIComponent(scopes.join(" "))}`;
